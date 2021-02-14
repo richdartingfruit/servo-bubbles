135888d11752dbd6f8679f0e31d9ea13d14126bb
richdartingfruit <76633464+richdartingfruit@users.noreply.github.com>
2021-02-13 19:39:46 -0800

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,16 +1,53 @@
+function goFan () {
+    while (true) {
+        angle = angle + 5
+        if (angle >= 100) {
+            break;
+        }
+        servos.P0.setAngle(angle)
+        basic.pause(200)
+    }
+    led.stopAnimation()
+}
 input.onButtonPressed(Button.A, function () {
-    angle = Math.max(15, angle - 5)
-    pins.servoWritePin(AnalogPin.P0, angle)
-    led.stopAnimation()
+    start = true
 })
+function fanStop () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
 input.onButtonPressed(Button.B, function () {
-    angle = Math.min(100, angle + 5)
-    pins.servoWritePin(AnalogPin.P0, angle)
-    led.stopAnimation()
+    start = false
 })
+function goDip () {
+    while (true) {
+        angle = angle - 5
+        if (angle <= 15) {
+            break;
+        }
+        servos.P0.setAngle(angle)
+        basic.pause(200)
+    }
+    led.stopAnimation()
+}
+function fanStart () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+}
 let angle = 0
+let start = false
+start = false
 angle = 15
 pins.servoWritePin(AnalogPin.P0, angle)
 basic.forever(function () {
     basic.showNumber(angle)
+})
+basic.forever(function () {
+    while (start == true) {
+        goDip()
+        basic.pause(200)
+        goFan()
+        basic.pause(100)
+        fanStart()
+        basic.pause(5000)
+        fanStop()
+    }
 })
